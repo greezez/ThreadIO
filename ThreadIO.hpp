c44f9dda1f29226136e7c8726192c8a1f0ae084a
Greego <kirill.grigo@gmail.com>
2021-12-27 04:42:36 +0300

    .

--- ThreadIO.hpp
+++ ThreadIO.hpp
@@ -6,6 +6,43 @@ namespace ThreadIO
 	namespace details
 	{
 
+		template <typename T, typename Tg>
+		class ThreadIOBase
+		{
+
+			using Type = T;
+			using TagType = Tg;
+
+		public:
+			ThreadIOBase()
+			{
+			}
+
+			~ThreadIOBase()
+			{
+			}
+
+		private:
+
+		};
+
 	}
+
+
+	class ThreadIO
+	{
+	public:
+		ThreadIO()
+		{
+		}
+
+		~ThreadIO()
+		{
+		}
+
+	private:
+
+	};
+
 
 }
