3dc27f15c29fe37a6fcdfc6e345c6bf2ffb72cfb
Greego <kirill.grigo@gmail.com>
2022-01-08 22:12:01 +0300

    .

--- ThreadIO.hpp
+++ ThreadIO.hpp
@@ -7,8 +7,63 @@
 namespace ThreadIO
 {
 
+	enum class ErrorCodeList : uint8_t
+	{
+		Success = 0,
+		BadAlloc,
+	};
+
+	class ErrorCode
+	{
+	public:
+
+		ErrorCode() : code(ErrorCodeList::Success)
+		{
+		}
+
+		~ErrorCode()
+		{
+		}
+
+	private:
+
+		ErrorCodeList code;
+
+	};
+
+
 	namespace details
 	{
+
+		template<typename T>
+		class List
+		{
+
+			struct Node
+			{
+				Node* next{nullptr};
+				T item{};
+			};
+
+		public:
+
+			List(size_t size, ErrorCode& ec) : cuurent(nullptr), head(nullptr), tail(nullptr)
+			{
+			}
+
+			~List()
+			{
+			}
+
+		private:
+
+			Node* cuurent;
+
+			Node* head;
+			Node* tail;
+
+		};
+
 
 	}
 
