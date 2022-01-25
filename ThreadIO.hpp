d757e2fbd4a90e32b8081153bb072ea9eae54649
Greego <kirill.grigo@gmail.com>
2022-01-25 20:06:23 +0300

    .

--- ThreadIO.hpp
+++ ThreadIO.hpp
@@ -14,6 +14,7 @@ namespace ThreadIO
 	};
 
 
+
 	class ErrorCode
 	{
 	public:
@@ -33,6 +34,7 @@ namespace ThreadIO
 	};
 
 
+
 	namespace details
 	{
 
@@ -48,32 +50,124 @@ namespace ThreadIO
 
 		public:
 
-			List(size_t size, ErrorCode& ec) : current(nullptr), head(nullptr), tail(nullptr)
+
+			List(size_t size, ErrorCode& ec) : current_(nullptr), head_(nullptr), size_(0)
 			{
 			}
+
 
 			~List()
 			{
 			}
 
-			T& current() {}
 
-			updateCurrent() {}
+			T& current() 
+			{
+				return current_->item;
+			}
 
-			bool addAfterCurrent() {}
-			bool deleteAfterCurrent() {}
+			T& front()
+			{
+				return head_->item;;
+			}
 
-			void resetCurrent() {}
+			void popFront()
+			{
+				if (head_ == nullptr)
+					return;
+
+				Node* node = head_;
+				head_ = node->next;
+
+				size_--;
+
+				delete node;
+			}
+
+			void updateCurrent() noexcept
+			{
+				if (current_->next != nullptr)
+				{
+					current_ = current_->next;
+					return;
+				}
+
+				current_ = head_;
+			}
+
+
+			bool addAndUpdateCurrent() noexcept
+			{
+				Node* node = createNode();
+
+				if (node == nullptr)
+					return false;
+
+				node->next = current_->next;
+				current_->next = node;
+				current_ = node;
+
+				return true;
+			}
+
 
 			template<typename Fn>
-			void forEach(Fn fn) {}
+			void forEach(Fn fn) 
+			{
+				Node* node = head_;
+
+				while (node != nullptr)
+				{
+					fn(node->item);
+
+					node = node->next;
+				}
+
+			}
+
+
+			void clear() noexcept
+			{
+				Node* node = nullptr;
+
+				while (head_ != nullptr)
+				{
+					node = head_;
+					head_ = head_->next;
+
+					delete node;
+				}
+
+			}
+
+
+			size_t size() noexcept
+			{
+				return size_;
+			}
+
 
 		private:
 
-			Node* current;
+			Node* createNode() noexcept
+			{
+				Node* node = new(std::nothrow) Node;
 
-			Node* head;
-			Node* tail;
+				if (node == nullptr)
+					return nullptr;
+
+				size_++;
+
+				return node;
+			}
+
+
+		private:
+
+			Node* current_;
+			Node* head_;
+
+			size_t size_;
 
 		};
 
